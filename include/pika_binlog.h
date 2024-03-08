5ddbba9ec8ccf972aae66510ff3f9a7fd0638c2e
白鑫 <baixin@MacBook-Air.local>
2024-03-08 14:15:32 +0800

    pass db_id, rocksdb_id parameters

--- include/pika_binlog.h
+++ include/pika_binlog.h
@@ -52,7 +52,11 @@ class Binlog : public pstd::noncopyable {
   void Unlock() { mutex_.unlock(); }
 
   virtual pstd::Status Put(const std::string& item);
+
   pstd::Status IsOpened();
+
+  virtual pstd::Status Put(const std::string& item, uint32_t db_id, uint32_t rocksdb_id);
+
   virtual pstd::Status GetProducerStatus(uint32_t* filenum, uint64_t* pro_offset, uint32_t* term = nullptr, uint64_t* logic_id = nullptr);
   /*
    * Set Producer pro_num and pro_offset with lock
