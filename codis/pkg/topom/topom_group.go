28b258e502216b8933e0c77559e4fc9359f37453
baixin <baixin@360.cn>
2024-03-25 14:20:03 +0800

    add s3 config

--- codis/pkg/topom/topom_group.go
+++ codis/pkg/topom/topom_group.go
@@ -823,9 +823,9 @@ func (s *Topom) UploadManifestToS3(gid int, tid int, bucket string, filename str
 			})
 
 			//waiting for pika trans manifest
+
 			//bucket := "pika"
 			//filename := "db/db0/bz.cc"
-
 			file, err := os.Open("/Users/charlieqiao/Desktop/bz.cc")
 			//ioutil.ReadAll(strings.NewReader(params["manifest"]))
 			if err != nil {
@@ -833,7 +833,7 @@ func (s *Topom) UploadManifestToS3(gid int, tid int, bucket string, filename str
 				println(err)
 			}
 			defer file.Close()
-			//over
+			//waiting for pika trans manifest over
 
 			uploader := s3manager.NewUploader(sess)
 			_, err = uploader.Upload(&s3manager.UploadInput{
