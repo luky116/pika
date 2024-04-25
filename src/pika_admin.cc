e31580c35522d34bb180e129186ccf927ae00da8
wangshaoyi <wangshaoyi@360.cn>
2024-04-25 18:53:57 +0800

    support flushdb command

--- src/pika_admin.cc
+++ src/pika_admin.cc
@@ -569,6 +569,9 @@ void FlushallCmd::FlushAllWithoutLock() {
       return;
     }
     DoWithoutLock(db);
+#ifndef USE_S3
+    DoBinlog(g_pika_rm->GetSyncMasterDBs()[p_info]);
+#endif
   }
   if (res_.ok()) {
     res_.SetRes(CmdRes::kOk);
@@ -630,6 +633,9 @@ void FlushdbCmd::FlushAllDBsWithoutLock() {
     return;
   }
   DoWithoutLock();
+#ifndef USE_S3
+  DoBinlog();
+#endif
 }
 
 void FlushdbCmd::DoWithoutLock() {
