51407fd53a89c5f33fe49e8526b58de0c2206e2a
liuyuecai <liuyuecai@360.cn>
2024-05-17 16:38:20 +0800

    persistence enabled: true

--- tools/kubeblocks_helm/debug.sh
+++ tools/kubeblocks_helm/debug.sh
@@ -1,4 +1,4 @@
 # helm install pika ./pika && helm install pika-cluster ./pika-cluster
 
 helm template pika ./pika --output-dir ./output/pika
-#helm template pika-cluster ./pika-cluster --output-dir ./output/pika-cluster
+helm template pika-cluster ./pika-cluster --output-dir ./output/pika-cluster
