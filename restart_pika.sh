c7cee290835afc939a5bc30ddf4fd014677250be
liuyuecai <liuyuecai@360.cn>
2024-07-10 17:25:30 +0800

    add start shell

---
+++ restart_pika.sh
@@ -0,0 +1,19 @@
+if [ "$1" -eq 1 ]; then
+  cd /data01/yuecai/rocksdb
+  git pull
+
+  rm -rf /data01/yuecai/pika-start-time/buildtrees/Source/rocksdb/db
+  rm -rf /data01/yuecai/pika-start-time/buildtrees/Source/rocksdb/cloud
+  cp -rf /data01/yuecai/rocksdb/db /data01/yuecai/pika-start-time/buildtrees/Source/rocksdb
+  cp -rf /data01/yuecai/rocksdb/cloud /data01/yuecai/pika-start-time/buildtrees/Source/rocksdb
+  echo "update rocksdb success"
+
+  cd /data01/yuecai/pika-start-time
+  export USE_S3=1
+  ./build.sh
+  echo "rebuild pika success"
+fi
+
+rm -rf ./pika-cloud2
+cd /data01/yuecai/pika-start-time
+./output/pika -c conf/pika-3344-test.conf
